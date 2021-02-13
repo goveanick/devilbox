76926a19c5d982d1c3b6ef32a927d9de23e9cb52
goveanick <nick.govea@gartner.com>
2021-02-12 18:54:43 -0600

    jokes can now be searched

--- data/www/project-1/htdocs/index.php
+++ data/www/project-1/htdocs/index.php
@@ -2,15 +2,11 @@
     <head>
     </head>
     <body>
-        <h1>Jokes Page</h1>
-        <?php
-
-            include "db_connect.php";
-            include "retrieve_all.php";
-            echo "<h2>Show all Jokes with the word - quit</h2>";
-            include "search_jokes.php";
-
-            $mysqli->close();
-        ?>
+        <h1>Welcome to the Jokes Search Engine!</h1>
+        <form action="search_jokes.php">
+            <label for="keyword">Please enter a keyword to find any joke:</label><br>
+            <input type="text" id="keyword" name="keyword" placeholder="keyword"><br><br>
+            <input type="submit" value="Submit">
+        </form>
     </body>
 </html>
--- data/www/project-1/htdocs/search_jokes.php
+++ data/www/project-1/htdocs/search_jokes.php
@@ -1,13 +1,27 @@
-<?php
-    $sql = "SELECT JokeID, Joke_question, Joke_answer FROM jokes_table WHERE Joke_question LIKE '%quit%'";
-    $result = $mysqli->query($sql);
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            include "db_connect.php";
+            $keyword = $_GET["keyword"];
+            $sql = "SELECT JokeID, Joke_question, Joke_answer FROM jokes_table WHERE Joke_question LIKE '%". $keyword ."%'";
+            $result = $mysqli->query($sql);
 
-    if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "Joke ID: " . $row["JokeID"]. " - Joke Question: " . $row["Joke_question"]. " " . $row["Joke_answer"]. "<br>";
-    }
-    } else {
-    echo "0 results";
-    }
-?>
+            echo "<h2>Show all Jokes with the word - " . $keyword ."</h2>";
+
+            if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "Joke ID: " . $row["JokeID"]. " - Joke Question: " . $row["Joke_question"]. " " . $row["Joke_answer"]. "<br>";
+            }
+            } else {
+            echo "0 results";
+            }
+            $mysqli->close();
+        ?>
+        <form>
+            <br><input type="button" value="Go back!" onclick="history.back()">
+        </form>
+    </body>
+</html>
